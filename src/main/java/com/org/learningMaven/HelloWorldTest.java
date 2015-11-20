a938015d720d3d74a670ab55285474a53fb93ea3
suman2k22000@yahoo.com <suman2k22000@yahoo.com>
2015-11-19 19:05:35 -0600

    Initial commit

---
+++ src/main/java/com/org/learningMaven/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.org.learningMaven;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+	
+	@Test
+	public void test() {
+		System.out.println("Hello World");
+	}
+
+}
