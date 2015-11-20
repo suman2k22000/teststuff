091f45dce348dba939122c7861c34e578d894935
suman2k22000@yahoo.com <suman2k22000@yahoo.com>
2015-11-19 19:26:46 -0600

    test

---
+++ src/main/java/com/org/learningMaven/AnotherTest.java
@@ -0,0 +1,12 @@
+package com.org.learningMaven;
+
+import org.testng.annotations.Test;
+
+public class AnotherTest {
+	
+	@Test
+	public void test() {
+		System.out.println("Testing out .gitignore");
+	}
+
+}
--- src/main/java/com/org/learningMaven/HelloWorldTest.java
+++ src/main/java/com/org/learningMaven/HelloWorldTest.java
@@ -7,6 +7,7 @@ public class HelloWorldTest {
 	@Test
 	public void test() {
 		System.out.println("Hello World");
+		System.out.println("After adding project to github");
 	}
 
 }
